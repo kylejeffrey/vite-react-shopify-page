87c1eb5d559516df2faf07fec0dfa885f54f5530
Kyle Jeffrey <kylejeffrey@Kyles-MacBook-Pro.local>
2022-12-12 15:05:35 +0900

    made page elements more responsive

--- src/components/item_top/item_top.jsx
+++ src/components/item_top/item_top.jsx
@@ -7,24 +7,24 @@ export const ItemTop = () => {
       <div className="item-top-left">
         <p>
           <a href="#">Home</a> / <a href="#">Samples</a> /{" "}
-          <a href="#">Neurofunk</a> / <a href="#">Twisted-01</a>
+          <a href="#">Neurofunk</a> / <a href="#">Twisted</a>
         </p>
         <h1>Twisted</h1>
         <h5>Neurofunk</h5>
       </div>
       <div className="item-top-right">
         <span>
-          <a href="#">
+          <a href="#" id="add-icon">
             <i className="fa-solid fa-plus"></i>
           </a>
         </span>
         <span>
-          <a href="#">
+          <a href="#" id="heart-icon">
             <i className="fa-solid fa-heart"></i>
           </a>
         </span>
         <span>
-          <a href="#">
+          <a href="#" id="share-icon">
             <i className="fa-solid fa-share"></i>
           </a>
         </span>
